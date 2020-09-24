1b3fad6053a29aa9604d99db934b3cce4bbc17d2
Anjan-Shomodder <anjanshomodder18@gmail.com>
2020-09-24 18:15:43 +0600

    scroll back to top functionality has been added

--- src/pages/home/home_page.jsx
+++ src/pages/home/home_page.jsx
@@ -24,15 +24,24 @@ const CONTAINER = styled.div`
 `
 
 const HOME = () => {
+	const go_to_top = (event) => {
+		document.body.scrollTop = 0 // For Safari
+		document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
+		event.preventDefault()
+	}
+
 	return (
 		<CONTAINER>
 			<PRODUCT_COLLECTION />
-			{/* <PRODUCT_COLLECTION /> */}
-			{/* <PRODUCT_COLLECTION /> */}
-			{/* <div className="the__end"> */}
-			{/* you have reach the end{' '} */}
-			{/* <span>{`>>`}</span> */}
-			{/* </div>{' '} */}
+			<PRODUCT_COLLECTION />
+			<PRODUCT_COLLECTION />
+			<button
+				className="the__end"
+				onClick={go_to_top}
+			>
+				you have reach the end{' '}
+				<span>{`>>`}</span>
+			</button>{' '}
 		</CONTAINER>
 	)
 }
