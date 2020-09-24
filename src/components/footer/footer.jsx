6b03c52e9ff465f7d1967d6fed37d08358278c72
Anjan-Shomodder <anjanshomodder18@gmail.com>
2020-09-25 00:21:44 +0600

    footer_two has been styled and need some styling and should be converted to links

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -32,7 +32,46 @@ const FOOTER_ONE = styled.div`
 	}
 `
 
-const FOOTER_TWO = styled.div``
+const FOOTER_TWO = styled.div`
+	display: grid;
+	grid-template-columns: 1fr;
+	justify-items: center;
+	color: white;
+	background: rgba(18, 22, 61, 0.7245098723082983);
+	background: linear-gradient(
+		180deg,
+		rgba(18, 22, 61, 0.7245098723082983) 0%,
+		rgba(32, 32, 32, 1) 100%
+	);
+
+	.country {
+		justify-self: normal;
+		display: grid;
+		grid-template-columns: repeat(
+			auto-fit,
+			minmax(10rem, auto)
+		);
+		justify-items: center;
+		padding: 3rem 0;
+	}
+
+	.switch_account,
+	sign_out {
+		padding: 1rem 0;
+	}
+
+	.legals {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		padding: 2rem 0;
+
+		.other {
+			place-self: center;
+			padding: 4rem 0;
+			grid-column: 1/4;
+		}
+	}
+`
 
 const footer_one_data = [
 	'Amazon.com',
@@ -65,7 +104,51 @@ const FOOTER = () => {
 					</div>
 				))}
 			</FOOTER_ONE>
-			<FOOTER_TWO />
+			<FOOTER_TWO>
+				<div className="country">
+					<div className="language">
+						English
+					</div>
+					<div className="currency">
+						Bangladeshi Taka
+					</div>
+					<div>
+						Bangladesh
+						<span
+							role="img"
+							aria-label="xx"
+						>
+							&#127463;
+						</span>
+						<span
+							role="img"
+							aria-label="xx"
+						>
+							&#127465;
+						</span>
+					</div>
+				</div>
+				<div className="switch_account">
+					switch_account
+				</div>
+				<div className="sign_out">sign out</div>
+
+				<div className="legals">
+					<div className="condition">
+						condition
+					</div>
+					<div className="privacy">
+						privacy notice
+					</div>
+					<div className="ads">
+						internet based ads
+					</div>
+					<div className="other">
+						1996-2020, Amazon.com, Inc. or
+						its afflicates
+					</div>
+				</div>
+			</FOOTER_TWO>
 		</>
 	)
 }
